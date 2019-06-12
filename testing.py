a4df6952ae045b5b56eddb360d1085d23cfb894a
Jacqueline <jacquelinelars@gmail.com>
2019-06-11 21:48:33 -0400

    fix website tokenizing

--- testing.py
+++ testing.py
@@ -5,11 +5,7 @@ from spacy.tokenizer import Tokenizer
 from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
 import re
 import os
-# from LookUp import look_up
-# from CharNGram import char_n_gram
 
-# create a custom tokenizer that keep hypen words together ex. hat-trick
-# and keeps hashtag symbol together with its word ex. #yolo
 def custom_tokenizer(nlp):
     infix_re = re.compile(r'''[\,\?\;\‘\’\`\“\”\"\'~]''')
     modified_prefixes = tuple(x for x in nlp.Defaults.prefixes if x != '#')
@@ -27,13 +23,13 @@ class tasi_text:
         self._tag()
 
     def _spacy_setup(self): 
-    	nlp = spacy.load('es') # find how to disable syntactic parsing to speed up algorithm
+        nlp = spacy.load('es') # find how to disable syntactic parsing to speed up algorithm
         is_candidate_filter = lambda token: token.pos_ in ["VERB", "NOUN", "ADJ"] and token.is_stop == False # include PROPN?
         Token.set_extension("is_candidate", getter=is_candidate_filter) # create a new attribute to identify anglicism candidates
         Token.set_extension("is_anglicism", default=False)
         nlp.tokenizer = custom_tokenizer(nlp)
-    	return nlp(self.text)
-  	
+        return nlp(self.text)
+    
     def _tag(self):
         for token in self.spacy_text:
             if token._.is_candidate == True:
@@ -42,56 +38,19 @@ class tasi_text:
         for token in self.spacy_text:
             print(token.text, token.lemma_, token.pos_, token._.is_candidate, token._.is_anglicism) 
                 # send to main module 
-
-    def anglicisms(self, spacy_text):
-        self.tag(spacy_text)
-        # return list of anglicisms
-        return
-	
-    def annotate(self, text):
-
-        self.tag(spacy_text)
-        # write to output file
-        return
-
-	def evaluate(self, gold_standard):
-        self.tag(spacy_text)
-        # compare to gold standard 
-        return
-
-class decider:
-    def __init__(self):
-        ngram_weights = self._setup
-    def _setup(self):
-        ngram_weights
-        look_up
+    
 
 
-
-def main(argv):
-	tasi_text = tasi_text()
-    if argv[0] == '-a':
-        tasi_text.annotate(argv[1])
-    elif argv[0] == '-e':
-        tasi_text.evaluate(argv[1])
-    else:
-        tasi_text.annotate(argv[0])
-        tasi_text.evaluate(argv[1])
-    os.system('say "your program has finished"')
-
-if __name__ == '__main__':
-    setup_package()
-
-
+text1 = """El médico argentino Eduardo Sosa en el hat-trick de su twitter @yesyes y #happy en https://www.lanacion.com.ar/e y su esposa Edith gat@gmail.com fueron a dejar todo en la Argentina para mudarse con su familia a Bielorrusia, la ex república de la Unión Soviética que había sufrido las mayores consecuencias de la explosión de la central de Chernobyl, ubicada a pocos kilómetros de la frontera con Ucrania. """
+text2 = """El médico argentino Eduardo Sosa en hat-trick de su twitter @yesyes y #happy"""
+sample = tasi_text(text1)
 """
-class tasi_doc:
-    def __init__(self, text):
-        self.text = text
-        self.dataframe = self._dataSetUp()
-        self.nlp = spacy.load('es', tag=True, entity=True, stop=True, disable=["textcat", "parser"])
+doc = nlp(text)
+for token in doc:
+    print(token)
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.is_alpha, token.is_stop, token._.is_candidate)
 
-    def _dataSetUp(self):
-        nlp(text)
 
-        return() 
+   return() 
 """
