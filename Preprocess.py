789c0c4307a69d2a7942f0f1dbd2f7ddc78a1c94
Chelsea Wang <chelseawang1995@gmail.com>
2019-07-06 18:50:52 -0400

    Add files via upload

--- Preprocess.py
+++ Preprocess.py
@@ -10,6 +10,14 @@ from spacy.lang.tokenizer_exceptions import URL_PATTERN
 from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
 import re
 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+
 
 # create a custom tokenizer that keep hypen words together ex. hat-trick
 # and keeps hashtag symbol together with its word ex. #yolo
@@ -44,33 +52,17 @@ def custom_tokenizer_modified(nlp):
 
     # set a custom extension to match if token is a hashtag
     hashtag_getter = lambda token: token.text.startswith('#')
-    Token.set_extension('is_hashtag', getter=hashtag_getter)
+    Token.set_extension('is_hashtag', getter=hashtag_getter, force=True)
 
     return Tokenizer(nlp.vocab, prefix_search=prefix_re.search,
                      suffix_search=suffix_re.search,
                      infix_finditer=infix_re.finditer,
                      token_match=url_and_hashtag_re.match
                      )
-def filter_noninterested_text_modified(row):
-    Pos_tag = row["POS"]
-    NE = row["NE"]
-    Stop = row["Stop"]
 
-    # Ignore any POS tags that are not Noun, VERB, or Adjective
-    if(Pos_tag in ["NOUN", "VERB", "ADJ"] and
-        Stop == False and
-        NE == "O"):
-        return True
-    else:
-        return False
 
-def custom_tokenizer_to_df(nlp):
-    # Samples to run in python console or testing
-    text1 = u"""El médico argentino Eduardo Sosa en el hat-trick de su twitter @yesyes y #happy en 
-          https://www.lanacion.com.ar/e y su esposa Edith gat@gmail.com fueron a dejar todo en la Argentina para mudarse con su 
-          familia a Bielorrusia, la ex república de la Unión Soviética que había sufrido las mayores consecuencias de la 
-          explosión de la central de Chernobyl, ubicada a pocos kilómetros de la frontera con Ucrania. """
-    doc = nlp(text1)
+# write all the tokens extracted from text into a data frame
+def custom_tokenizer_to_df(doc):
 
     # Write the tokens to data frame
     df = pd.DataFrame()
@@ -79,27 +71,27 @@ def custom_tokenizer_to_df(nlp):
     df["POS"] = [token.pos_ for token in doc]
     df["NE"] = [token.ent_iob_ for token in doc]
     df["Lemma"] = [token.lemma_ for token in doc]
-    df['Canidate'] = df.apply(filter_noninterested_text, axis=1) # what is a candidate may be interesting to linguists
     df["Anglicism"] = np.nan
     return df
 
-def filter_noninterested_text(row):
-    token = row["Token"]
-    Pos_tag = row["POS"]
-    Name_entity = row["NE"]
-    Anglicism = row["Anglicism"]
 
-    # Ignore any POS tags that are not Noun, VERB, or Adjective
-    if(Pos_tag != "NOUN" & Pos_tag != "VERB" | Pos_tag != "ADJ"):
-        Anglicism = "No"
-    # Ignore Stop Words
-    elif(token.is_stop):
-        Anglicism = "No"
-    # Ignore NEs
-    elif(Name_entity):
-        Anglicism = "No"
+# filter out non interested text by three criteria below
+def filter_noninterested_text(nlp, df):
+    for i in df.Token.index:
+        # Ignore any POS tags that are not Noun, VERB, or Adjective
+        if(df.loc[i,"POS"] not in ["NOUN", "VERB", "ADJ"]):
+            df.loc[i, 'Language'] = df.loc[i,'POS']
+            df.loc[i, 'Anglicism'] = "No"
+        # Ignore Stop Words
+        elif(nlp.vocab[df.loc[i,'Token']].is_stop):
+            df.loc[i, 'Language'] = "stop word"
+            df.loc[i, 'Anglicism'] = "No"
+        # Ignore NEs
+        elif(df.loc[i,'NE'] != "O"):
+            df.loc[i, 'Language'] = "name entity"
+            df.loc[i, 'Anglicism'] = "No"
 
-    return row
+    return df
 
 
 def main():
@@ -109,12 +101,22 @@ def main():
     # Tokenize text using custom tokenizer
     nlp.tokenizer = custom_tokenizer_modified(nlp)
 
-    # Paste in text and write token into data frame
-    NACC_df = custom_tokenizer_to_df(nlp)
-    NACC_df.to_csv(r'spacy-annotated_df.csv', index=None, header=True)
+    # Samples to run in python console or testing
+    text1 = u"""El médico argentino Eduardo Sosa en el hat-trick de su twitter @yesyes y #happy en 
+          https://www.lanacion.com.ar/e y su esposa Edith gat@gmail.com fueron a dejar todo en la Argentina para mudarse con su 
+          familia a Bielorrusia, la ex república de la Unión Soviética que había sufrido las mayores consecuencias de la 
+          explosión de la central de Chernobyl, ubicada a pocos kilómetros de la frontera con Ucrania. """
+    doc = nlp(text1)
+
+    # write token into data frame
+    NACC_df = custom_tokenizer_to_df(doc)
 
     # Filter out non interested tokens by assigning label
-    # Not done yet, don't run filter_noninterested_text function
+    filter_noninterested_text(nlp, NACC_df)
+
+    NACC_df.to_csv(r'spacy-annotated_df.csv', index=None, header=True)
+
+
 
 if __name__ == '__main__':
     main()
