cb844f75fcfa2f7336382a2be05614601018f046
Chelsea Wang <chelseawang1995@gmail.com>
2019-07-23 14:14:52 -0400

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -12,22 +12,15 @@ from spacy.lang.tokenizer_exceptions import URL_PATTERN
 from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
 import re
 
-# this version still leaves extra whitespace, not sure why
-def cleanText_new(text):
-    # get rid of newlines
-    regex = r"\s\s+|\n|\r"
-    subst = " "
-    cleaned = re.subn(regex, subst, text, re.MULTILINE)
-    return cleaned
+
 
 # clean text before spacy
 def cleanText(text):
-    # get rid of newlines
+    # get rid of new line terminator
     text = text.strip().replace("\n", " ").replace("\r", " ").replace("\r\n", " ").replace("  ", " ")
     return text
 
 # deal with more symbols to seperate tokens
-# deal with url and hashtag
 def custom_tokenizer_modified(nlp):
     # spacy defaults: when the standard behaviour is required, they
     # need to be included when subclassing the tokenizer
@@ -37,28 +30,31 @@ def custom_tokenizer_modified(nlp):
     extended_suffixes = tuple(list(nlp.Defaults.suffixes) + ["-"])
     suffix_re = compile_suffix_regex(extended_suffixes)
 
-    # extending the default url regex with regex for hashtags with "or" = |
+    # extending the default url regex
     url = URL_PATTERN
     url_re = re.compile(url)
-
     return Tokenizer(nlp.vocab, prefix_search=prefix_re.search,
                      suffix_search=suffix_re.search,
                      infix_finditer=infix_re.finditer,
                      token_match=url_re.match
                      )
 
-
 # write all the tokens extracted from text into a data frame
 def custom_tokenizer_to_df(nlp, doc):
+    # Initialize the Matcher with a vocab
     matcher = Matcher(nlp.vocab)
 
+    ###############################################################
     # Add pattern for valid hashtag, i.e. '#' plus any ASCII token
     matcher.add("HASHTAG", None, [{"ORTH": "#"}, {"IS_ALPHA": True}])
 
-    # Register token extension
+    # Register token extension for hashtag
     Token.set_extension("is_hashtag", default=False)
 
+    # Fit in text in matcher
     matches = matcher(doc)
+
+    # Find hashtag and merge, assign hashtag label
     hashtags = []
     for match_id, start, end in matches:
         if doc.vocab.strings[match_id] == "HASHTAG":
@@ -68,6 +64,46 @@ def custom_tokenizer_to_df(nlp, doc):
             retokenizer.merge(span)
             for token in span:
                 token._.is_hashtag = True
+    ##############################################################
+
+    ##############################################################
+    # Find number and merge, assign number label
+    # Add pattern for valid hashtag, i.e. '#' plus any ASCII token
+    matcher.add("LONG_NUMBER", None, [{"IS_DIGIT": True}, {"ORTH": ','}, {"IS_DIGIT": True}])
+    matcher.add("LONG_NUMBER", None, [{"IS_DIGIT": True}, {"ORTH": '.'}, {"IS_DIGIT": True}])
+
+    # Register token extension for hashtag
+    Token.set_extension("is_long_number", default=False)
+
+    # Fit in text in matcher
+    matches = matcher(doc)
+
+    long_number = []
+    for match_id, start, end in matches:
+        if doc.vocab.strings[match_id] == "LONG_NUMBER":
+            long_number.append(doc[start:end])
+    with doc.retokenize() as retokenizer:
+        for span in long_number:
+            retokenizer.merge(span)
+            for token in span:
+                token._.is_long_number = True
+    ##############################################################
+
+    for i, token in enumerate(doc):
+        if token._.is_hashtag:
+            token.tag_ = 'Hashtag'
+        if token.like_url:
+            token.tag_ = 'URL'
+        if token.like_email:
+            token.tag_ = 'Email'
+        if token.is_stop:
+            token.tag_ = 'Stop Word'
+        if token.like_num:
+            token.tag_ = 'Number'
+        if token._.is_long_number:
+            token.tag_ = 'Number'
+        if token.is_punct:
+            token.tag_ = 'Punctuation'
 
     # Write the tokens to data frame
     df = pd.DataFrame()
@@ -75,20 +111,23 @@ def custom_tokenizer_to_df(nlp, doc):
     df['POS'] = [token.pos_ for token in doc]
     df['NE'] = [token.ent_iob_ for token in doc]
     df['Lemma'] = [token.lemma_ for token in doc]
+    df['Tag'] = [token.tag_ for token in doc]
     df['Language'] = np.nan
     df['Candidate'] = True
     df['Anglicism'] = np.nan
+    return df
 
+def filter_noninterested_text(df):
     # filter out non interested text by three criteria below
     for i in df.Token.index:
-        # Ignore any POS tags that are not Noun, VERB, or Adjective
-        if(df.loc[i,"POS"] not in ['NOUN', 'VERB', 'ADJ']):
-            df.loc[i, 'Language'] = df.loc[i,'POS']
+        # Ignore bad words
+        if(df.loc[i,"POS"] in ['Hashtag', 'URL', 'Email', 'Stop Word', 'Number', 'Punctuation']):
+            df.loc[i, 'Language'] = df.loc[i, 'Tag']
             df.loc[i, 'Candidate'] = False
             df.loc[i, 'Anglicism'] = False
-        # Ignore Stop Words
-        elif(nlp.vocab[df.loc[i,'Token']].is_stop):
-            df.loc[i, 'Language'] = 'Stop Word'
+        # Ignore any POS tags that are not Noun, VERB, or Adjective
+        elif(df.loc[i,"POS"] not in ['NOUN', 'VERB', 'ADJ']):
+            df.loc[i, 'Language'] = df.loc[i,'POS']
             df.loc[i, 'Candidate'] = False
             df.loc[i, 'Anglicism'] = False
         # Ignore NEs
@@ -108,8 +147,8 @@ def main():
 
     # Samples to run in python console or testing
 
-    #text = open("Data/OpinionArticles-text.txt", encoding="utf8").read()
-    text = open("Data/Sample-text.txt", encoding="utf8").read()
+    text = open("Data/OpinionArticles-text.txt", encoding="utf8").read()
+    #text = open("Data/Sample-text.txt", encoding="utf8").read()
 
     # clean text
     clean_text = cleanText(text)
@@ -121,10 +160,13 @@ def main():
     text_df = custom_tokenizer_to_df(nlp, doc)
 
     # update user on length of tokens
-    print("Processing %s word document" % len(text_df))
+    print("Processing %s word document" % len(doc))
+
+    # Filter out non interested tokens by assigning label
+    filter_noninterested_text(text_df)
 
     # write df to csv
-    text_df.to_csv(r'Data/Sample-TASI.csv', index=None, header=True)
+    text_df.to_csv(r'Data/OpinionArticles-TASI.csv', index=None, header=True)
 
     #open annotated file
     #subprocess.call(['open',r'Data/OpinionArticles-TASI.csv'])
